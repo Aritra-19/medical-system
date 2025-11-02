2dcc906fbe7ab40c64f88a881fa1db545600a5f7
user.name "Aritra Bag <user.email "aritrabag5427@gmail.com>
2025-11-02 22:00:28 +0530

    Updated Files

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -24,27 +24,41 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         return token
     
+# In api/serializer.py
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
+    date_of_birth = serializers.DateField(required=True)
+    phone_number = serializers.CharField(required=True)
+    
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2']
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name', 'date_of_birth', 'phone_number']
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError(
-                {"password": "password fields does not match"}
-            )
+            raise serializers.ValidationError({"password": "password fields do not match"})
         return attrs
         
     def create(self, validated_data):
+        profile_data = {
+            'first_name': validated_data.pop('first_name'),
+            'last_name': validated_data.pop('last_name'),
+            'date_of_birth': validated_data.pop('date_of_birth'),
+            'phone_number': validated_data.pop('phone_number'),
+        }
+        validated_data.pop('password2')
+        
         user = User.objects.create(
-            username = validated_data['username'],
-            email = validated_data['email'],
+            username=validated_data['username'],
+            email=validated_data['email'],
         )
-                
         user.set_password(validated_data['password'])
         user.save()
-
+        
+        # Update the auto-created profile
+        Profile.objects.filter(user=user).update(**profile_data)
+        
         return user
